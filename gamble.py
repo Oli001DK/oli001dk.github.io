6d426ae0d28de5eecabb89cad1f76b332530a7c6
Oli001DK <147048155+Oli001DK@users.noreply.github.com>
2023-10-06 10:51:02 +0200

    gam

--- gamble.py
+++ gamble.py
@@ -2,7 +2,7 @@ MAX_LINE = 3
 def deposit():
     while True:
 
-        penge = input("hvor manget vil du deposit?" )
+        penge = input("hvor meget vil du deposit?" )
         if penge.isdigit():
             penge = int(penge)
             if penge > 0:
@@ -12,4 +12,20 @@ def deposit():
         else:
             print("det kan være et nummer")
     return penge
-deposit()
+
+def lines():
+    while True:
+
+        lines = input("hvor mange liner vil du spille på? (1-" + str(MAX_LINE) + ")? ")
+        if lines.isdigit():
+            lines = int(lines)
+            if 1 <= lines <= MAX_LINE:
+                break
+            else:
+                print("det kan ikke vaere 0") 
+        else:
+            print("det kan være et nummer")
+    return lines
+
+def main():
+    bal = deposit()
