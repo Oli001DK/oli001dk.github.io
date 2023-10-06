69fe03303c40749cade61fb86d3b2bb4888d796c
Oli001DK <147048155+Oli001DK@users.noreply.github.com>
2023-10-06 10:07:07 +0200

    psuh

---
+++ gamble.py
@@ -0,0 +1,8 @@
+MAX_LINE = 3
+def deposit():
+    penge = input("hvor manget vil du deposit?" )
+    if penge == 0:
+        print("det kan ikke vaere 0") 
+    else:
+        break
+    if penge == int(penge)
