0206f156c1eb1e2b2f09dcde2693c8acea1e2e48
Oli001DK <147048155+Oli001DK@users.noreply.github.com>
2023-10-26 12:37:41 +0000

    su

--- ash.py
+++ ash.py
@@ -1,6 +1,7 @@
 import requests
 
 URL = 'https://api.hypixel.net/skyblock/bazaar'
+
 response = requests.get(URL)
 data = response.json()
 print(data)
--- blackjack.py
+++ blackjack.py
@@ -1,4 +1,6 @@
 import random
+KORT = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11]
+
 Total_penge = random.randint(1000,2000)
 
 def Deposit():
@@ -16,3 +18,18 @@ def Deposit():
     return gamble
 
 Deposit()
+hand1 = random.randint(2,11)
+hand2 = random.randint(2,11)
+if hand1 and hand2 == 22:
+    print("Du har tabt")
+    Deposit()
+hand = hand1 + hand2    
+
+dealerhand1 = random.randint(2,11)
+dealerhand2 = random.randint(2,11)
+if dealerhand1 and dealerhand2 == 22:
+    print("Dealer har tabt")
+    Deposit()
+print("du har " + str(hand))
+print("dealer har " + str(dealerhand1))
+print("Vil du har ")
