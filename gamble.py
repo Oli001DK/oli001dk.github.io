0091c937388323236a53ff60c21df99333fddf6c
Oli001DK <147048155+Oli001DK@users.noreply.github.com>
2023-10-26 08:10:25 +0200

    banan crak

--- gamble.py
+++ gamble.py
@@ -1,7 +1,6 @@
 MAX_LINE = 3
 def deposit():
     while True:
-
         penge = input("hvor meget vil du deposit?" )
         if penge.isdigit():
             penge = int(penge)
@@ -10,12 +9,11 @@ def deposit():
             else:
                 print("det kan ikke vaere 0") 
         else:
-            print("det kan være et nummer")
+            print("det skal være et nummer")
     return penge
 
 def lines():
     while True:
-
         lines = input("hvor mange liner vil du spille på? (1-" + str(MAX_LINE) + ")? ")
         if lines.isdigit():
             lines = int(lines)
