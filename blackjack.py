2cde2f7c1faeaac560c9b001e9fd9b925e6d5f31
Oli001DK <147048155+Oli001DK@users.noreply.github.com>
2023-10-26 14:38:41 +0200

    Create blackjack.py
    d

---
+++ blackjack.py
@@ -0,0 +1,35 @@
+import random
+KORT = [2,2,2,2,3,3,3,3,4,4,4,4,5,5,5,5,6,6,6,6,7,7,7,7,8,8,8,8,9,9,9,9,10,10,10,10,11,11,11,11]
+
+Total_penge = random.randint(1000,2000)
+
+def Deposit():
+    while True:
+        print("hvor meget vil du gamble? du har " + str(Total_penge))
+        gamble = input()
+        if gamble.isdigit():
+            gamble = int(gamble)
+            if gamble > 0:
+                break
+            else:
+                print("det kan ikke vaere 0") 
+        else:
+            print("det kan være et nummer")
+    return gamble
+
+Deposit()
+hand1 = random.randint(2,11)
+hand2 = random.randint(2,11)
+if hand1 and hand2 == 22:
+    print("Du har tabt")
+    Deposit()
+hand = hand1 + hand2    
+
+dealerhand1 = random.randint(2,11)
+dealerhand2 = random.randint(2,11)
+if dealerhand1 and dealerhand2 == 22:
+    print("Dealer har tabt")
+    Deposit()
+print("du har " + str(hand))
+print("dealer har " + str(dealerhand1))
+print("Vil du har ")
