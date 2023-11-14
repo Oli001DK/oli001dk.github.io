81d2decc72ac509f6ec9cde25915a20182c5ed81
Oli001DK <147048155+Oli001DK@users.noreply.github.com>
2023-11-14 20:53:19 +0000

    s

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
 import requests
-URL = "https://api.slothpixel.me/api/skyblock/bazaar/HOT_POTATO_BOOK"
-response = requests.get(URL)
+response = requests.get('https://api.slothpixel.me/api/skyblock/bazaar/MOLTEN_POWDER', timeout=1)
 data = response.json()
-buy_summary = data.get("buy_summary")
-print(buy_summary)
+buy_summary = data.get("sell_summary")
+if buy_summary:
+    prices_per_unit = [summary['pricePerUnit'] for summary in buy_summary]
+    sorted_prices = sorted(prices_per_unit, reverse=True)
+    if len(sorted_prices) >= 4:
+        molten_powder = sum(sorted_prices[:4]) / 4
+        molten_powder_rounded = round(molten_powder)
+        print(molten_powder_rounded, "Sell order")
