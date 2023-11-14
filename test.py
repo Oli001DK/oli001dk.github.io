ba434c4fdabf63be7accaf529c9af447df39c1d6
Oli001DK <147048155+Oli001DK@users.noreply.github.com>
2023-11-14 21:00:35 +0000

    dd

--- test.py
+++ test.py
@@ -1,11 +1,11 @@
 import requests
-response = requests.get('https://api.slothpixel.me/api/skyblock/bazaar/MOLTEN_POWDER', timeout=1)
-data = response.json()
-buy_summary = data.get("sell_summary")
+molten_powder_response = requests.get('https://api.slothpixel.me/api/skyblock/bazaar/MOLTEN_POWDER', timeout=1)
+molten_powder_data = molten_powder_response.json()
+buy_summary = molten_powder_data.get("sell_summary")
 if buy_summary:
-    prices_per_unit = [summary['pricePerUnit'] for summary in buy_summary]
-    sorted_prices = sorted(prices_per_unit, reverse=True)
-    if len(sorted_prices) >= 4:
-        molten_powder = sum(sorted_prices[:4]) / 4
+    molten_powder_price = [summary['pricePerUnit'] for summary in buy_summary]
+    molten_powder_sorted = sorted(molten_powder_price, reverse=True)
+    if len(molten_powder_sorted) >= 4:
+        molten_powder = sum(molten_powder_sorted[:4]) / 4
         molten_powder_rounded = round(molten_powder)
-        print(molten_powder_rounded, "Sell order")
+        print(molten_powder_rounded, "Pr item in  Sell order")
