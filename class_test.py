2c0a4bd564193d520abf5cddddddfae2fe0371c8
Oli001DK <147048155+Oli001DK@users.noreply.github.com>
2023-11-28 10:30:26 +0000

    x

--- class_test.py
+++ class_test.py
@@ -1,25 +1,22 @@
-
 import requests
 
-class Api:
+
+class Api: 
     def __init__(self, name_api):
         self.name_api = name_api
 
-def get_api(self):
-        api_loot = requests.get(self.name_api)
-        api_buy_summary = api_loot.get("sell_summary")
-        if api_buy_summary:
-            api_price = [summary['pricePerUnit'] for summary in api_buy_summary]
-            api_sorted = sorted(api_price, reverse=True)
-            if len(api_sorted) >= 4:
-                api = sum(api_sorted[:4]) / 4
-                api_rounded = round(api)
+    def get_hypixel_api(self):
+        api_loot = requests.get(self.name_api, timeout = 1)
+        api_buy_summary = api_loot.json().get("buy_summary")
+        api_price = [summary['pricePerUnit'] for summary in api_buy_summary]
+        api_sorted = sorted(api_price, reverse=True)
+        api = sum(api_sorted[:4]) / 4
+        api_rounded = round(api)
         return api_rounded
-    
+
     def __str__(self):
-        return str(self.get_api())
+        return str(self.get_hypixel_api())
 
 api_1 = Api('https://api.slothpixel.me/api/skyblock/bazaar/MOLTEN_POWDER')
-api_1.get_api()
+api_1.get_hypixel_api()
 print(api_1)
-
